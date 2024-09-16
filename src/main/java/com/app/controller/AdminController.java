fd97029ec797fba5cba8603837befde57144533a
ayushhunt <ayushoo7hunt@gmail.com>
2024-09-17 00:31:11 +0530

    working on controller

--- src/main/java/com/app/controller/AdminController.java
+++ src/main/java/com/app/controller/AdminController.java
@@ -1,5 +1,24 @@
 package com.app.controller;
 
+
+import com.app.entity.User;
+import com.app.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/admin/")
 public class AdminController {
+
+    private UserService userService;
+
+    //Comment this code after one run for single root user
+    @PostMapping("/createAdmin")
+    public ResponseEntity<String> saveAdmin(@RequestBody User user) {
+        userService.addUser(user);
+        String str = "Admin created successfully";
+        return ResponseEntity.ok(str);
+    }
+
 
 }
